0661da2225f5d5d594f40a6f17366fb3a8f15c72
Di Jin <332548128@qq.com>
2018-09-26 01:16:50 -0500

    video list list_item detail added to show a video on page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React,{Component}from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import YTSearch from 'youtube-api-search';
 import reducers from './reducers';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyApSf9o_R3Fwnh5ZqlvsikAHUnJjkUaKLQ';
+import VideoDetail from './components/video_detail';
 
-const App = ()=>{
-    return (<div>
-        <SearchBar />
-    </div>);
+class App extends Component{
+    constructor(props)
+    {
+        super();
+        this.state={videos:[]};
+
+        YTSearch({key: API_KEY, term:'asmr'},(videos)=>{  //get the video function
+
+            this.setState({videos});
+            console.log(this.state.videos);
+        });
+    }
+
+    render() {
+        return (<div>
+            <SearchBar/>
+            <VideoDetail video={this.state.videos[0]}/>
+            <VideoList videos={this.state.videos} />
+        </div>);
+    }
+
 }
 
 ReactDOM.render(
